cabc20532deca86f35d044c90b585f6c36e7a731
刘超 <liuchao@xiaoshouyi.com>
2017-06-15 16:41:15 +0800

    无用提交

--- flatListScrollw/demo.js
+++ flatListScrollw/demo.js
@@ -7,9 +7,12 @@ import {
     TouchableHighlight,
     Dimensions,
     PanResponder,
+    ViewPagerAndroid,
+    LayoutAnimation,
+    UIManager
 } from 'react-native';
 
-import ViewPagerAndroid from '../XsyAndroidViewPager';
+// import ViewPagerAndroid from '../XsyAndroidViewPager';
 
 const window = Dimensions.get('window');
 
@@ -25,96 +28,102 @@ class Demo extends React.Component {
         }
         this.state = {
             Responder: true,
-            childMarginTop: 0,
-            scrollOnTouch: false
         }
         this.Responder = true;
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.caseInEaseOut);
+
     }
 
 
+    onWheel() {
+        alert('nihao')
+    }
 
     render() {
         return (
             <ScrollView
-                onContentSizeChange={(contentWidth, contentHeight) => this.scrollHeight = contentHeight}
+                onContentSizeChange={(contentWidth, contentHeight) => {
+                    this.scrollViewContentHeight = contentHeight;
+                    console.log('flatListDemonum-onscroll-ContentSizeChange', this.scrollViewContentHeight);
+                }}
+                onTouchStart
+                stickyHeaderIndices={[1]}
                 overScrollMode={'never'}
                 onMomentumScrollEnd={(e) => {
-                    if (!this.state.scrollOnTouch) {
-                        this.setState({
-                            childMarginTop: e.nativeEvent.contentOffset.y - 1000
-                        })
-                    }
-
-                    console.log('iuojlk', e.nativeEvent.contentOffset.y);
-                }}
-                scrollEnabled={this.state.Responder}
-                onScrollShouldSetResponder={() => { return false }}
-                onResponderReject={() => { return false }}
-                onStartShouldSetResponderCapture={() => { return false }}
-                onStartShouldSetResponder={() => { return false }}
-                onScroll={(e) => {
-                    this.scrollHeighted = e.nativeEvent.contentOffset.y;
-                    {/*console.log('iuojlk', e.nativeEvent.contentOffset.y);*/ }
-                    {/*if (this.scrollHeight > 0 && e.nativeEvent.contentOffset.y > 1020) {
+                    if (e.nativeEvent.contentOffset.y > (200 + window.height) && this.state.Responder) {
                         this.setState({
                             Responder: false
                         })
-                    }*/}
+                    } else if (e.nativeEvent.contentOffset.y < (200 + window.height) && !this.state.Responder) {
+                        this.setState({
+                            Responder: true
+                        })
+                    }
+                    console.log('flatListDemonum-onscroll-onMomentumScrollEnd', e.nativeEvent.contentOffset.y);
+                }}
+                scrollEnabled={this.state.Responder}
+                onScroll={(e) => {
+                    this.scrollHeighted = e.nativeEvent.contentOffset.y;
+                    if (e.nativeEvent.contentOffset.y > (200 + window.height)) {
+                        this.setState({
+                            Responder: false
+                        })
+                    } else if (!this.state.Responder) {
+                        this.setState({
+                            Responder: true
+                        })
+                    }
+                    console.log('flatListDemonum-onscroll-scrollView', e.nativeEvent.contentOffset.y);
                 }}
             >
-                <TouchableHighlight>
-                    <View style={{ flex: 1, height: 500, backgroundColor: 'green' }} />
-                </TouchableHighlight>
-                <View style={{ flex: 1, height: 500, backgroundColor: 'yellow' }} />
+                <View style={{ flex: 1, height: 100, backgroundColor: 'green' }} />
+                <View style={{ flex: 1, height: 100, backgroundColor: 'yellow' }} />
                 <ViewPagerAndroid
-                    style={[!this.state.scrollOnTouch ? { flex: 0 } : { flex: 1, height: window.height, }]}
+                    style={{ flex: 1, height: window.height * 2 }}
                     onPageSelected={(event) => {
-                        if (event.nativeEvent.position != 0) {
+                        if (!this.state.Responder) {
                             this.setState({
-                                childMarginTop: 0,
-                                scrollOnTouch: true,
-                            })
-                        } else {
-                            this.setState({
-                                scrollOnTouch: false,
+                                Responder: true,
                             })
                         }
-                        console.log('flatListDemonumsdfsdf', event.nativeEvent.position);
                     }}
                 >
-                    <FlatList
-                        ref={(FlatList) => this.FlatList = FlatList}
-                        data={this.dataMap}
-                        onContentSizeChange={(contentWidth, contentHeight) => {
-                            console.log('flatListDemonum', contentHeight);
-                        }}
-                        renderItem={({ item }) => {
-                            this.num = this.num + 1;
-                            return (
-                                <View style={{ height: 300, width: window.width, alignItems: 'center', flexDirection: 'row' }}>
-                                    <Text style={{ width: 70, textAlign: 'center' }} >{item.name}</Text>
-                                </View>
-                            )
-                        }}
-                        onScroll={(e) => {
-                            if (e.nativeEvent.contentOffset.y < 40) {
-                                this.setState({
-                                    Responder: true
-                                })
-                            };
-                            console.log('iuojlk-lkill', e.nativeEvent.contentOffset.y);
-                        }}
-                        keyExtractor={(item, number) => number}
-                        getItemLayout={(data, index) => ({ length: 300.5, offset: 300.5 * index, index })}
-                        ItemSeparatorComponent={() => <View style={{ width: window.width, height: 0.5, backgroundColor: 'green' }} />}
-                        ListHeaderComponent={() => <View style={{ height: 50, width: window.width, backgroundColor: 'pink', alignItems: 'center', justifyContent: 'center' }}>
-                            <Text>我是头部组件</Text>
-                        </View>}
-                        onEndReached={() => { }}
-                        onEndReachedThreshold={10}
-                        refreshing={false}
-                        initialNumToRender={1}
-                    />
+                    <View style={{ flex: 1 }}>
+                        <FlatList
+                            ref={(FlatList) => this.FlatList = FlatList}
+                            data={this.dataMap}
+                            onMomentumScrollEnd={(e) => {
+                                console.log('flatListDemonum-scrollend', e.nativeEvent.contentOffset.y);
+                            }}
+                            renderItem={({ item }) => {
+                                return (
+                                    <View style={{ height: 300, width: window.width, alignItems: 'center', flexDirection: 'row' }}>
+                                        <Text style={{ width: 70, textAlign: 'center' }} >{item.name}</Text>
+                                    </View>
+                                )
+                            }}
+                            onScroll={(e) => {
+                                if (e.nativeEvent.contentOffset.y > window.height) {
+                                    this.setState({
+                                        Responder: false
+                                    })
+                                } else if (!this.state.Responder) {
+                                    this.setState({
+                                        Responder: true
+                                    })
+                                }
+                                console.log('flatListDemonum-onScroll', e.nativeEvent.contentOffset.y);
+                            }}
+                            keyExtractor={(item, number) => number}
+                            getItemLayout={(data, index) => ({ length: 300.5, offset: 300.5 * index, index })}
+                            ItemSeparatorComponent={() => <View style={{ width: window.width, height: 0.5, backgroundColor: 'green' }} />}
+                            onEndReached={() => { }}
+                            onEndReachedThreshold={10}
+                            refreshing={false}
+                            initialNumToRender={1}
+                        />
+                    </View>
                     <View style={{ backgroundColor: 'blue', width: window.width, height: window.height }}>
                         <View style={{ width: window.width, backgroundColor: 'red', height: 500, marginTop: this.state.childMarginTop }} />
                     </View>
